4cb5f94e4c260ca64eb2028ca2b7425e1c948d93
Ben <pretty.fiendish@gmail.com>
2021-04-15 15:24:00 +0100

    adding css elements for the header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return <header>
+    <div className="parallelogram">
+    </div>
+  </header>
+};
+
+export default Header;
